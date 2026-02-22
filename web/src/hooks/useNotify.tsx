a467cfd11115c54cb8391140366f0d3aad074e30
Ash Goren <ash@mortalwombat.net>
2026-02-22 00:15:59 -0800

    move success toast to top center

--- web/src/hooks/useNotify.tsx
+++ web/src/hooks/useNotify.tsx
@@ -10,7 +10,7 @@ export const useNotify = () => {
   return {
     toastSuccess: (message: string) => enqueueSnackbar(message, {
       variant: 'success',
-      anchorOrigin: { vertical: 'top', horizontal: 'right' },
+      anchorOrigin: { vertical: 'top', horizontal: 'center' },
       action: (snackbarId) => (
         <>
           <Button size='small' color='inherit' onClick={undo}>
